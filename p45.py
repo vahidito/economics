b4e925eab16fce1c50909778f43c9e28e92dca49
Vahidito <vahidito@gmail.com>
2020-03-15 19:57:52 +0330

    p 45 is added

---
+++ p45.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+ts_length = 100
+ϵ_values = []
+i = 0
+while i < ts_length:
+    eee = np.random.randn()
+    ϵ_values. append(eee)
+    i = i + 1
+plt. plot( ϵ_values)
+plt. show()
