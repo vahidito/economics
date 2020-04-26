a38a67082b9ee6e0fac8be59102ca99f01817e69
v.mirzaei <vahidito@gmail.com>
2020-04-26 12:43:12 +0430

    p54

--- p54.py
+++ p54.py
@@ -5,10 +5,10 @@ def binomial_rv(n, p):
     count = 0
     for i in range(n):
         u = uniform()
-    if u < p:
-        count = count + 1  # Or count += 1
+        if u < p:
+            count = count + 1  # Or count += 1
     return count
 
 
-mmm = binomial_rv(10, 0.5)
-print(mmm)
+print(binomial_rv(10, 0.5))
+
